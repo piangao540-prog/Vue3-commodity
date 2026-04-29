59b65e0100e7af2cfd42f71aca95b733aeff3a29
wc <iii@qq.com>
2026-04-29 21:26:38 +0800

    路由设计

---
+++ src/views/Home/index.vue
@@ -0,0 +1,2 @@
+<template>我是home</template>
+<script setup lang="ts"></script>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+    <h1>我是首页</h1>
+    <!-- 二级路由 -->
+     <RouterView/>
+    </div>
+
+</template>
