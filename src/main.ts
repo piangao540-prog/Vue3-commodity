689763a2c16269dcffb248699217c3f4e8f14aae
wc <iii@qq.com>
2026-05-02 14:42:10 +0800

    二级分类-路由滚动体验优化

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
@@ -15,6 +14,13 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(layzPlugin)
+
+// 禁用 v-infinite-scroll 的废弃警告
+app.config.warnHandler = (msg, instance, trace) => {
+  if (!msg.includes('v-infinite-scroll is about to be deprecated')) {
+    console.warn(msg, instance, trace)
+  }
+}
 
 
 
