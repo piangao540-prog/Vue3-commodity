7ec062bfb5bb1c9738d7e95442d348460273fc5f
wc <iii@qq.com>
2026-05-03 19:23:00 +0800

    表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,19 @@
 <script setup>
+import {ref} from 'vue'
+const from = ref({
+    account:'',
+    password:''
+})
 
+const rules = {
+    account:[
+        {required:true,message:'用户名不能为空',trigger:'blur'}
+    ],
+    password:[
+        {required:true,message:'密码不能为空',trigger:'blur'},
+        {min:6,max:14,message:'密码位数为6~14',trigger:'blur'}
+    ]
+}
 </script>
 
 
@@ -24,13 +38,13 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="from" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="from.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="from.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
