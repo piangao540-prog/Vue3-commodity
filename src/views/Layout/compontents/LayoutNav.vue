6a47e81831915238ac45965611874f3267cc5bca
wc <iii@qq.com>
2026-05-04 13:54:23 +0800

    登录-使用token适配多模版显示

--- src/views/Layout/compontents/LayoutNav.vue
+++ src/views/Layout/compontents/LayoutNav.vue
@@ -1,5 +1,6 @@
 <script setup>
-import router from '@/router';
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -9,8 +10,8 @@ import router from '@/router';
         <!-- 多模版渲染 区分登录状态和非登录状态 -->
 
         <!-- 适配思路: 登录时显示第一块 非登录时显示第二块  是否有token -->
-        <template v-if="false">
-          <li><a href="javascript:;"><i class=" iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class=" iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
