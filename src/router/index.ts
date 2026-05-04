e57184e9f7cb53c2734757426549b20321d6659a
wc <iii@qq.com>
2026-05-04 22:36:18 +0800

    列表购物车-基础数据渲染

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import home from '@/views/Home/index.vue'
 import category from '@/views/Category/index.vue'
 import subcategory from '@/views/SubCategroy/index.vue'
 import detail from '@/views/Detail/index.vue'
+import cartList from '@/views/cartList/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
         path: 'detail/:id',
         name: 'detail',
         component: detail
+      },
+      {
+        path: 'cartList',
+        name: 'cartList',
+        component: cartList
       }]
     },
     {
