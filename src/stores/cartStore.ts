aca8dd7c535a7537af0e3ec49d8b52adc0f57ad4
wc <iii@qq.com>
2026-05-05 11:56:21 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -8,10 +8,12 @@ interface CartItem {
     name?: string
     price?: number
     picture?: string
+    selected?: boolean
 }
 
 export const useCartStore = defineStore('cart', () => {
     const cartList = ref<CartItem[]>([])
+
     const addCart = (goods: CartItem) => {
         const item = cartList.value.find((item) => goods.skuId === item.skuId)
         if (item) {
@@ -20,11 +22,20 @@ export const useCartStore = defineStore('cart', () => {
             cartList.value.push(goods)
         }
     }
+
     const delCart = (skuId: string) => {
         const index = cartList.value.findIndex((item) => skuId === item.skuId)
         cartList.value.splice(index)
     }
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * (c.price ?? 0), 0))
-    return { cartList, addCart, delCart, allCount, allPrice }
-})
+
+    const singleCheck = (skuId: string, selected: boolean) => {
+        const item = cartList.value.find((item) => item.skuId === skuId)
+        if (item) {
+            item.selected = selected
+        }
+    }
+
+    return { cartList, addCart, delCart, allCount, allPrice, singleCheck }
+}, { persist: true })
