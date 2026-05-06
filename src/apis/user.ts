4903ba4ea75b7ce01335a7ae6a147185ccada6a7
wc <iii@qq.com>
2026-05-06 23:40:39 +0800

    会员中心-个人信息渲染

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -2,12 +2,21 @@ import httpInstance from "@/utils/https";
 
 export const loginApi = (account: string, password: string) => {
     return httpInstance({
-        url:'/login',
-        method:'POST',
-        data:{
+        url: '/login',
+        method: 'POST',
+        data: {
             account,
             password
         }
-    
-    });
-};
+
+    })
+}
+
+export const getLikeListApi = ({ limit = 4 }) => {
+    return httpInstance({
+        url: '/goods/relevant',
+        params: {
+            limit
+        }
+    })
+}
--- src/views/Member/Components/UserInfo.vue
+++ src/views/Member/Components/UserInfo.vue
@@ -1,5 +1,16 @@
 <script setup>
-const userStore = {}
+import { useUserStore } from '@/stores/user'
+import { getLikeListApi } from '@/apis/user';
+import {onMounted, ref} from 'vue'
+import GoodsItem from '@/views/Home/components/GoodsItem.vue'
+const likeList = ref([])
+const userStore = useUserStore()
+const getLikeList = async () =>{
+    const res = await getLikeListApi({limit:4})
+    console.log(res)
+    likeList.value = res.data.result
+}
+onMounted(() => getLikeList())
 </script>
 
 <template>
@@ -32,7 +43,7 @@ const userStore = {}
         <h4 data-v-bcb266e0="">猜你喜欢</h4>
       </div>
       <div class="goods-list">
-        <!-- <GoodsItem v-for="good in likeList" :key="good.id" :good="good" /> -->
+        <GoodsItem v-for="good in likeList" :key="good.id" :good="good" />
       </div>
     </div>
   </div>
