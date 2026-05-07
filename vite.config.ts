2f67923d863c999fd72abe6af4160016115f509a
wc <iii@qq.com>
2026-05-07 18:32:52 +0800

    配置GitHub Pages部署

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 添加这一行，用于 GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
